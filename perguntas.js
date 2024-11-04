36c4fa9fd9e790bee8054bebae753d61772f8f41
Analirt24 <00001137247368sp@aluno.educacao.sp.gov.br>
2024-11-04 18:00:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+criaCartao (
+    'Programação',
+    'O que é Python?',
+    'O JavaScript é uma linguagem de programaçaõ'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual é a capital da França?',
+    'A capital da França é Paris' 
+)
+
+criaCartao (
+    'Programação',
+    'O que é uma função',
+   'Uma função de codigo que executa alguma tarefa'
+)
+
+criaCarts (
+    'Lingua inglesa',
+    'Como se diz oi em ingles?',
+    'Oi em ingles é HI(RAI)'
+)
+
+    
